a264f16825661924f8ac45d8e7bc373626f97762
botelja <ivo.surjak@gmail.com>
2019-05-12 07:20:09 +0200

    Solved on 12-05-19

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -3,7 +3,7 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 const reverseString = function(s) {
-  s = s.reverse();
+  s.reverse();
 };
 
 //console.log(reverseString(["h","e","l","l","o"]), ["o","l","l","e","h"]);
