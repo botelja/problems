8311f78dc9374e637db8e9a97eb2381ceb276b58
botelja <ivo.surjak@gmail.com>
2019-05-12 07:17:09 +0200

    Solved on 12-05-19

---
+++ reverse-string/reverse-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+const reverseString = function(s) {
+  s = s.reverse();
+};
+
+//console.log(reverseString(["h","e","l","l","o"]), ["o","l","l","e","h"]);
